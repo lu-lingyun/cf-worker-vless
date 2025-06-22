42dc7480506c8a4e3e6dad1787970646402fed5f
John Doe <johndoe@example.com>
2025-06-22 22:21:45 +0800

    a

--- index.js
+++ index.js
@@ -174,8 +174,8 @@ async function 建立传输管道(WS接口, TCP接口, 写入初始数据) {
 
   // 保活机制
   setInterval(async () => {
-    await 传输数据.write(new Uint8Array(0));
-    await WS接口.send(new Uint8Array(0));
+    await 传输数据.write(new Uint8Array([0]));
+    await WS接口.send(new Uint8Array([0]));
   }, 10000);
 }
 
