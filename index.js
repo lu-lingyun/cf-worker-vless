4eecc07a0e0a9699eb546ffd4caa6767345e6213
水灵 <1668589925@qq.com>
2025-07-12 19:09:18 +0800

    优化

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ async function 建立传输管道(WS接口, TCP接口, 写入初始数据) {
   // TCP数据转发到WebSocket
   (async () => {
     while (true) {
-      const { value, done } = await 读取数据.read();
+      const { done, value } = await 读取数据.read();
       if (done) break;
       WS接口.send(value);
     }
