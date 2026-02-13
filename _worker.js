a66e96ca047ea05d57c02cccb8e2fd57eafe9cd1
水灵 <1668589925@qq.com>
2026-02-13 11:44:53 +0800

    1

--- _worker.js
+++ _worker.js
@@ -62,13 +62,14 @@ async function 启动传输管道(WS接口) {
     let 地址信息索引 = 提取地址索引 + 1;
 
     switch (识别地址类型) {
-      case 1:
+      case 1: {
         地址长度 = 4;
         访问地址 = new Uint8Array(
           VL数据.slice(地址信息索引, 地址信息索引 + 地址长度),
         ).join(".");
         break;
-      case 2:
+      }
+      case 2: {
         地址长度 = new Uint8Array(
           VL数据.slice(地址信息索引, 地址信息索引 + 1),
         )[0];
@@ -77,6 +78,7 @@ async function 启动传输管道(WS接口) {
           VL数据.slice(地址信息索引, 地址信息索引 + 地址长度),
         );
         break;
+      }
       case 3: {
         地址长度 = 16;
         const dataView = new DataView(
